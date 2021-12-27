5be1ca2f3df9280ad0b713d27c6446c1b8a24275
earl-stephens <stephensearl@hotmail.com>
2021-12-26 23:55:41 -0700

    Add method to ask for user to change color

--- src/application/App.java
+++ src/application/App.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Scanner;
+
 public class App {
 	public static void main(String[] args) {
 		/* Coding challenge
@@ -12,6 +14,17 @@ public class App {
 		 * methods: change color, minimize/maximize, reposition
 		 */
 		
-		
+		Window window1 = new Window();
+		System.out.println("Enter one of the numbers below to select that window color: ");
+		String[] colorOptions = window1.getColorOptions();
+		for(int i = 0; i < colorOptions.length; i++) {
+			System.out.printf(" %d. %s\n", (i+1), colorOptions[i]);
+		}
+		Scanner scanner = new Scanner(System.in);
+		int userInput = scanner.nextInt();
+		scanner.close();
+		window1.setWindowColor(userInput - 1);
+		String newColor = window1.getWindowColor();
+		System.out.println("The new window color is " + newColor);
 	}
 }
